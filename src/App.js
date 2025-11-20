edfc5455bfccd5a2e7012b80cd59f7cc7d4495dc
mt <73151258+SYS-Kevin@users.noreply.github.com>
2025-11-20 09:08:36 +0000

    Add count logic and learn useEffect function

--- src/App.js
+++ src/App.js
@@ -1,19 +1,29 @@
+import { useEffect } from "react";
 import { useState } from "react";
 
 export default function App() {
   const [data, setData] = useState("");
+  const [count, setCount] = useState(0);
 
   async function getData() {
     const res = await fetch("https://api.adviceslip.com/advice");
     const data = await res.json();
     //console.log(data.slip.advice);
     setData(data.slip.advice);
+    setCount((c) => c + 1);
   }
+
+  useEffect(function () {
+    getData();
+  }, []);
 
   return (
     <div>
       <h1>{data}</h1>
-      <button onClick={getData}>Get Data Here</button>
+      <button onClick={getData}>Get news Here</button>
+      <p>
+        You have read <strong>{count}</strong> pieces news.
+      </p>
     </div>
   );
 }
